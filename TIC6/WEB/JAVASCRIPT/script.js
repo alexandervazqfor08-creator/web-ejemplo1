b13eb66cb5f54b5918335fd3144bd56fa4da2a68
alexandervazqfor08-creator <alexandervazqfor.08@gmail.com>
2026-01-21 09:33:52 -0600

    Estructura inicial del proyecto web

---
+++ TIC6/WEB/JAVASCRIPT/script.js
@@ -0,0 +1,7 @@
+//obtener el boton por ID que se puso
+const boton1=document.getElementById('tilin');
+
+// Agrega un evento al boton
+boton1.addEventListener('click',function(){
+      alert("YEAHH");
+}); 
